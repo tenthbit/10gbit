4055860360dc5b4431162fd310d3daeb22785d3c
Daniel Lamando <dan@danopia.net>
2013-07-15 14:25:30 -0400

    Handle sending and receiving messages

--- app.js
+++ app.js
@@ -20,12 +20,9 @@ function readHandler (stream, result) {
   if (pkt.op == 'welcome') {
     showAuth(pkt.ex.server, pkt.ex.auth[0]);
   } else if (pkt.op == 'ack') {
-    let msg = new Gtk.MessageDialog({text: pkt.ex.for + ' worked :D'});
-    msg.set_transient_for(rootWin);
-    msg.set_destroy_with_parent(true);
-    msg.add_button('gtk-ok', Gtk.ResponseType.ACCEPT).grab_default();
-    msg.run();
-    msg.destroy();
+    addLine('Authenticated to server');
+  } else if (pkt.op == 'act') {
+    addLine('<' + pkt.sr + '> ' + pkt.ex.data);
   } else {
     print('unhandled op ' + pkt.op);
   };
@@ -34,7 +31,7 @@ function readHandler (stream, result) {
 inStr.read_bytes_async(1024*5, 0, null, readHandler);
 
 /*
- * Show authentication dialog
+ * Dialogs
  ************************/
 function showAuth (server, method) {
   let window = new Gtk.Dialog({title: "Log in to " + server, modal: true});
@@ -67,17 +64,64 @@ function showAuth (server, method) {
   window.destroy();
 }
 
+function addLine (line) {
+  let doc = webView.get_dom_document();
+  let p = doc.create_element('p');
+  p.inner_text = line;
+  doc.get_element_by_id('backlog').append_child(p);
+  p.scroll_into_view(false);
+}
+
+function dialog (message) {
+  let msg = new Gtk.MessageDialog({text: message});
+  msg.set_transient_for(rootWin);
+  msg.set_destroy_with_parent(true);
+  msg.add_button('gtk-ok', Gtk.ResponseType.ACCEPT).grab_default();
+  msg.run();
+  msg.destroy();
+}
+
 
 /*
  * Run the main thang
  ************************/
 Gtk.init(null, 0);
 
-var rootWin = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL, border_width: 200});
+var rootWin = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL, border_width: 10});
 rootWin.title = "10Gbit";
+rootWin.set_default_size(800, 600);
 rootWin.connect("destroy", function () { Gtk.main_quit(); });
 
+let webView = new imports.gi.WebKit.WebView();
+webView.connect('close-web-view', function () { Gtk.main_quit(); });
+webView.load_string("<section id='backlog'></section>", 'text/html', 'utf-8', 'about:blank');
+
+let scroller = new Gtk.ScrolledWindow();
+scroller.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC);
+scroller.add(webView);
+
+let msgTxt = new Gtk.Entry({activates_default: true});
+let msgBtn = new Gtk.Button({label: 'Send', can_default: true});
+let msgBox = new Gtk.HBox();
+msgBox.pack_start(msgTxt, true, true, 0);
+msgBox.pack_end(msgBtn, false, false, 0);
+
+msgBtn.connect('clicked', function () {
+  let msg = msgTxt.get_text();
+  outStr.write(JSON.stringify({op: 'act', rm: '48557f95', ex: {type: 'msg', data: msg}}) + '\n', null);
+  msgTxt.set_text('');
+});
+
+let rootBox = new Gtk.VBox();
+rootBox.pack_start(scroller, true, true, 5);
+rootBox.pack_end(msgBox, false, false, 0);
+
+rootWin.add(rootBox);
 rootWin.show_all();
+
+msgTxt.grab_focus();
+msgBtn.grab_default();
+
 Gtk.main();
 
 outStr.write(JSON.stringify({op: 'leave'}) + '\n', null);
